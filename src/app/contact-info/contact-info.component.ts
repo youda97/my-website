48acef17492a3417c9170673f83e23330aae4d48
Yousef Ouda <ouda.yousef@gmail.com>
2019-09-24 17:57:38 -0400

    Add contact info

---
+++ src/app/contact-info/contact-info.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "app-contact-info",
+	templateUrl: "./contact-info.component.html",
+	styleUrls: ["./contact-info.component.scss"]
+})
+export class ContactInfoComponent {
+}
